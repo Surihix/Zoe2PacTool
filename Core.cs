8d828416297b96227130786025a05ee23373f0b9
Surihix <neweramysterymailer@gmail.com>
2025-06-10 07:34:51 +0530

    moved project to .net 6

--- Core.cs
+++ Core.cs
@@ -1,105 +1,113 @@
-﻿using Zoe2PacTool;
-using Zoe2PacTool.PacCommon;
+﻿using Zoe2PacTool.PacCommon;
 using Zoe2PacTool.PacRepack;
 
-
-static void ValidityCheck(string filePathVar)
+namespace Zoe2PacTool
 {
-    using (FileStream checkFile = new(filePathVar, FileMode.Open, FileAccess.Read))
+    internal class Core
     {
-        using (BinaryReader checkFileReader = new(checkFile))
+        static void Main(string[] args)
         {
-            var zpacStr = "";
-            CmnMethods.GenerateString(checkFileReader, 0, 4, ref zpacStr);
-
-            if (!zpacStr.Contains("PAC"))
+            try
             {
-                CmnMethods.ErrorExit("This is not a valid ZOE2 atlas pac file.");
+                if (args.Length < 3)
+                {
+                    Console.WriteLine("Enough arguments not specified");
+                    Help.ShowAppCommands();
+                }
+
+                var specifiedVersion = args[0].Replace("-", "");
+                var specifiedAction = args[1].Replace("-", "");
+                var inFileFolder = args[2];
+
+                var gameVersion = CmnEnums.VersionsList.mars;
+                if (Enum.TryParse(specifiedVersion, false, out CmnEnums.VersionsList convertedGameVersion))
+                {
+                    gameVersion = convertedGameVersion;
+                }
+                else
+                {
+                    CmnMethods.ErrorExit("Specified game version was invalid. has to be either -mars or -hd");
+                }
+
+                var toolAction = ToolActionSwitches.u;
+                if (Enum.TryParse(specifiedAction, false, out ToolActionSwitches convertedToolAction))
+                {
+                    toolAction = convertedToolAction;
+                }
+                else
+                {
+                    CmnMethods.ErrorExit("Specified tool action was invalid. has to be either -c or -d");
+                }
+
+                switch (toolAction)
+                {
+                    case ToolActionSwitches.u:
+                        CmnMethods.FileFolderExistsCheck(inFileFolder, CmnEnums.ExistsCheckType.file);
+                        ValidityCheck(inFileFolder);
+                        PacUnpack.ArchiveUnpack(inFileFolder, gameVersion);
+                        break;
+
+                    case ToolActionSwitches.r:
+                        if (args.Length < 4)
+                        {
+                            CmnMethods.ErrorExit("atlas pac file to repack is not specified in the argument. specify this file along with the extracted folder");
+                        }
+                        var inPacFile = args[3];
+                        CmnMethods.FileFolderExistsCheck(inFileFolder, CmnEnums.ExistsCheckType.folder);
+                        CmnMethods.FileFolderExistsCheck(inPacFile, CmnEnums.ExistsCheckType.file);
+                        ValidityCheck(inPacFile);
+
+                        switch (gameVersion)
+                        {
+                            case CmnEnums.VersionsList.mars:
+                                PacRpkMARS.ArchiveMars(inFileFolder, inPacFile);
+                                break;
+
+                            case CmnEnums.VersionsList.hd:
+                                PacRpkHD.ArchiveHd(inFileFolder, inPacFile);
+                                break;
+                        }
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                CmnMethods.FileFolderExistsDel("CrashLog.txt", CmnEnums.ExistsCheckType.file);
+
+                using (StreamWriter logWriter = new("CrashLog.txt"))
+                {
+                    logWriter.WriteLine(ex);
+                }
+
+                Console.WriteLine("Crash details recorded in CrashLog.txt file");
+                Console.WriteLine("");
+                CmnMethods.ErrorExit("" + ex);
             }
         }
-    }
-}
 
 
-try
-{
-    if (args.Length < 3)
-    {
-        Console.WriteLine("Enough arguments not specified");
-        Help.ShowAppCommands();
-    }
-
-    var specifiedVersion = args[0].Replace("-", "");
-    var specifiedAction = args[1].Replace("-", "");
-    var inFileFolder = args[2];
-
-    var gameVersion = CmnEnums.VersionsList.mars;
-    if (Enum.TryParse(specifiedVersion, false, out CmnEnums.VersionsList convertedGameVersion))
-    {
-        gameVersion = convertedGameVersion;
-    }
-    else
-    {
-        CmnMethods.ErrorExit("Specified game version was invalid. has to be either -mars or -hd");
-    }
-
-    var toolAction = ToolActionSwitches.u;
-    if (Enum.TryParse(specifiedAction, false, out ToolActionSwitches convertedToolAction))
-    {
-        toolAction = convertedToolAction;
-    }
-    else
-    {
-        CmnMethods.ErrorExit("Specified tool action was invalid. has to be either -c or -d");
-    }
-
-    switch (toolAction)
-    {
-        case ToolActionSwitches.u:
-            CmnMethods.FileFolderExistsCheck(inFileFolder, CmnEnums.ExistsCheckType.file);
-            ValidityCheck(inFileFolder);
-            PacUnpack.ArchiveUnpack(inFileFolder, gameVersion);
-            break;
-
-        case ToolActionSwitches.r:
-            if (args.Length < 4)
+        static void ValidityCheck(string filePathVar)
+        {
+            using (FileStream checkFile = new(filePathVar, FileMode.Open, FileAccess.Read))
             {
-                CmnMethods.ErrorExit("atlas pac file to repack is not specified in the argument. specify this file along with the extracted folder");
+                using (BinaryReader checkFileReader = new(checkFile))
+                {
+                    var zpacStr = "";
+                    CmnMethods.GenerateString(checkFileReader, 0, 4, ref zpacStr);
+
+                    if (!zpacStr.Contains("PAC"))
+                    {
+                        CmnMethods.ErrorExit("This is not a valid ZOE2 atlas pac file.");
+                    }
+                }
             }
-            var inPacFile = args[3];
-            CmnMethods.FileFolderExistsCheck(inFileFolder, CmnEnums.ExistsCheckType.folder);
-            CmnMethods.FileFolderExistsCheck(inPacFile, CmnEnums.ExistsCheckType.file);
-            ValidityCheck(inPacFile);
+        }
 
-            switch (gameVersion)
-            {
-                case CmnEnums.VersionsList.mars:
-                    PacRpkMARS.ArchiveMars(inFileFolder, inPacFile);
-                    break;
 
-                case CmnEnums.VersionsList.hd:
-                    PacRpkHD.ArchiveHd(inFileFolder, inPacFile);
-                    break;
-            }
-            break;
+        enum ToolActionSwitches
+        {
+            u,
+            r
+        }
     }
-}
-catch (Exception ex)
-{
-    CmnMethods.FileFolderExistsDel("CrashLog.txt", CmnEnums.ExistsCheckType.file);
-
-    using (StreamWriter logWriter = new("CrashLog.txt"))
-    {
-        logWriter.WriteLine(ex);
-    }
-
-    Console.WriteLine("Crash details recorded in CrashLog.txt file");
-    Console.WriteLine("");
-    CmnMethods.ErrorExit("" + ex);
-}
-
-enum ToolActionSwitches
-{
-    u,
-    r
 }
